99b50bd4bbe963b6ae55cf39178c97e14a3724c8
mattia98fn <mattia98fn@live.it>
2017-02-01 02:58:47 -0500

    Modifiche

--- log_in.php
+++ log_in.php
@@ -4,71 +4,75 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Login</title>
-	<link href="menu.css" rel="stylesheet" />
-</head>
-<body>
-	<body bgcolor = "00BFFF"/>
-	
-	<nav align = "center">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="registrazione.php">Registrati</a></li>
-			<li><a href="#">Area Privata</a>
-				<ul>
-					<li><a href="log_in.php">Accedi</a></li>
-				</ul>
-			</li>
-		</ul>
-	</nav>
-	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-		<center>
-			<form id="login" action="" method="POST">
-				<table>
-					<tr>
-						<td><b>Username:</b></td>
-						<td><input type="text" name="username"></td>
-					</tr>
-					<tr>
-						<td><b>Password:</b></td>
-						<td><input type="password" name="password"></td>
-					</tr>
-				</table>
-				<br/>
-				<input type="submit" name="invia" value="Invia"><br/><br/>
-			</form>
+   <head>
+      <title>Login</title>
+      <link href="menu.css" rel="stylesheet" />
+      <!-- Funzioni JAVASCRIPT -->
+      <script type="text/javascript">
+         function erroreDati() {
+            alert("ERRORE NELL'INSERIMENTO DEI DATI OPPURE UTENTE NON REGISTRATO");
+         }
+      </script>
+   </head>
+   <body>
+      <body bgcolor = "00BFFF"/>
+      <!-- Menu per navigazione -->
+      <nav align = "center">
+         <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="registrazione.php">Registrati</a></li>
+            <li><a href="#">Area Privata</a>
+               <ul>
+                  <li><a href="log_in.php">Accedi</a></li>
+               </ul>
+            </li>
+         </ul>
+      </nav>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <!-- Form per login -->
+         <center>
+            <form id="login" action="" method="POST">
+               <table>
+                  <tr>
+                     <td><b>Username:</b></td>
+                     <td><input type="text" name="username"></td>
+                  </tr>
+                  <tr>
+                     <td><b>Password:</b></td>
+                     <td><input type="password" name="password"></td>
+                  </tr>
+               </table>
+               <br/>
+               <input type="submit" name="invia" value="Invia"><br/><br/>
+            </form>
+            <!-- PHP -->
+            <?php 
+               if(isset($_POST['username']))
+               {
+                  try
+                  {
+                     $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $db, $username, $password);
+                     $stm = $dbh->prepare("SELECT * FROM Utenti WHERE Username = :user AND Password = :passwd");
+                     $stm->bindValue(':user',$_POST['username']);
+                     $pw = md5($_POST['password']);
+                     $stm->bindValue(':passwd',$pw);
+                     $stm->execute();
+                     if($stm->rowCount() == 1)
+                     {
+                     }
+                     else
+                     {
+                        echo '<script type="text/javascript">erroreDati();</script>';
+                     }
+                  }
+                  catch(PDOException $e)
+                  {
+                     echo 'Errore nella connessione al DataBase<br/>';
+                  }
+               }
+            ?>
 
-			<?php 
-				if(isset($_POST['username']))
-				{
-					try
-					{
-						$dbh = new PDO('mysql:host=' . $host . ';dbname=' . $db, $username, $password);
-						$stm = $dbh->prepare("SELECT * FROM users WHERE username = :user AND password = :passwd");
-						$stm->bindValue(':user',$_POST['username']);
-						$pw = md5($_POST['password']);
-						$stm->bindValue(':passwd',$pw);
-						$stm->execute();
-						if($stm->rowCount() == 1)
-						{
-							echo '<form action="#" method="POST">
-									<input type="submit" name="areaP" value="Accedi all\'Area Personale">
-								  </form>';
-						}
-						else
-						{
-							echo 'Dati errati<br/>';
-						}
-					}
-					catch(PDOException $e)
-					{
-						echo 'Errore nella connessione al DataBase<br/>';
-					}
-				}
-			?>
-
-			<br/>
-		</center>
-</body>
+            <br/>
+         </center>
+   </body>
 </html>
--- registrazione.php
+++ registrazione.php
@@ -6,16 +6,22 @@
 	<head>
 		<title>Registrazione</title>
 		<link href="menu.css" rel="stylesheet" />
+      <!-- Funzioni JAVASCRIPT -->
 		<script type="text/javascript">	
 			function registrazioneAvvenuta(){
-				alert("Registrazione avvenuta !");
+				alert("REGISTRAZIONE AVVENUTA !");
 			}
+         
+         function erroreCampi(){
+            alert("RIEMPIRE TUTTI I CAMPI !");
+         }
+         
 		</script>
 	</head>
 	<body>
 		
 		<body bgcolor = "00BFFF"/>
-		<!-- Menu --> 
+		<!-- Menu per navigazione--> 
 		<nav align = "center">
 			<ul>
 				<li><a href="index.php">Home</a></li>
@@ -27,8 +33,8 @@
 			</ul>
 		</nav>
 		
-		<br/><br/><br/><br/><br/><br/><br/><br/>
-		<!-- Form -->
+		<br/><br/><br/><br/><br/>
+		<!-- Form per registrazione-->
 		<center>
 			<fieldset>
 				<legend>
@@ -65,34 +71,40 @@
 							<td><input type="text" name="stato" id="input_user"></td>
 						</tr>			
 					</table>
-					<br/>
-					<input type="submit" name="btnInvia" value="Invia" id="submit_button">
+					<br/><br/>
+               <input type="submit" name="btnInvia" value="Invia" id="submit_button">
 				</form>
 			</fieldset>
-			
+			<!-- PHP -->
 			<?php 
 				if(isset($_POST['username']))
 				{
 					try
 					{
 						$dbh = new PDO('mysql:host=' . $host . ';dbname=' . $db, $username, $password);
-						$stm = $dbh->prepare("INSERT INTO users(username,password,nome,cognome,dataNascita,citta,stato) VALUES(:user,:passwd,:nome,:cognome,:dataN,:citta,:stato)");
-						$stm->bindValue(':user',$_POST['username']);
-						$pw = md5($_POST['password']);
-						$stm->bindValue(':passwd',$pw);
-						$stm->bindValue(':nome',$_POST['nome']);
-						$stm->bindValue(':cognome',$_POST['cognome']);
-						$stm->bindValue(':dataN',$_POST['dataNascita']);
-						$stm->bindValue(':citta',$_POST['citta']);
-						$stm->bindValue(':stato',$_POST['stato']);
-						$stm->execute();
-						if($stm->errorCode() == 0)
-						{
-							echo '<script type="text/javascript">registrazioneAvvenuta()</script>';
-						}
-						else{
-							echo 'Errore nella Query';
-						}
+                  
+                  if($_POST['username']=="" OR $_POST['password']=="" OR $_POST['nome']=="" OR $_POST['cognome']="" OR $_POST['citta']=="" OR $_POST['stato']=="") {
+                     echo '<script type="text/javascript">erroreCampi();</script>';
+                  }
+                  else {
+                     $stm = $dbh->prepare("INSERT INTO Utenti(Username,Password,Nome,Cognome,DataNascita,Citta,Stato) VALUES(:user,:passwd,:nome,:cognome,:dataN,:citta,:stato)");
+                     $stm->bindValue(':user',$_POST['username']);
+                     $pw = md5($_POST['password']);
+                     $stm->bindValue(':passwd',$pw);
+                     $stm->bindValue(':nome',$_POST['nome']);
+                     $stm->bindValue(':cognome',$_POST['cognome']);
+                     $stm->bindValue(':dataN',$_POST['dataNascita']);
+                     $stm->bindValue(':citta',$_POST['citta']);
+                     $stm->bindValue(':stato',$_POST['stato']);
+                     $stm->execute();
+                     if($stm->errorCode() == 0)
+                     {
+                        echo '<script type="text/javascript">registrazioneAvvenuta()</script>';
+                     }
+                     else{
+                        echo 'Errore nella Query';
+                     }
+                  }
 					}
 					catch (PDOException $e) 
 					{

56d6e08cbbe771fc190b75fe96181851a1083ab3
mattia98fn <mattia98fn@live.it>
2017-02-13 05:28:04 -0500

    Aggiunta combo box

--- area_privata.php
+++ area_privata.php
@@ -18,14 +18,23 @@
 				<li><a href="index.php">Home</a></li>
 				<li><a href="#">Profilo</a>
 					<ul>
-						<li>Info</li>
-					</ul>
-					<ul>
 						<li><a href="log_out.php">Log Out</a></li>
 					</ul>
 				</li>
 			</ul>
 		</nav>
 		<!-- Form -->
+		<form>
+			<select>
+				<?php
+					$mysqli = new mysqli("localhost","root","","RegistrazioneUtenti");
+					$query = $mysqli->query("SELECT idCategoria,NomeCategoria FROM Categoria");
+    			while($row=$query->fetch_row()){                                                 
+						echo "<option value='".$row[0]."'>".$row[1]."</option>";
+    			}
+				?>
+			</select>
+		</form>
+		
 	</body>
 </html>
